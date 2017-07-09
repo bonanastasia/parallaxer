d1035240d5c98e92e6ffe57cea6af7fcd9ed5423
Anastasia Goodwin <ana.goodwin10@gmail.com>
2017-07-08 23:42:13 -0500

    demo site updates

--- demoAssets/demo.js
+++ demoAssets/demo.js
@@ -8,6 +8,9 @@ var neil = document.querySelector('.neil');
 var neil_text = document.querySelector('.neil-text');
 var arrow = document.querySelector('.bouncing-arrow');
 var welcome = document.querySelector('.welcome-message');
+var moon_text = document.querySelector('.moon-text');
+var moon_arrow = document.querySelector('.moon-arrow');
+var stick_text = document.querySelector('.stick-text');
 
 var parallaxConfig = [{
   element: stars
@@ -25,12 +28,13 @@ var parallaxConfig = [{
 
 }, {
   element: neil,
-  offset: 2500,
-  distance: 4
-
+  offset: 9999,
+  onReveal: function() {
+    neil.classList.add('neil-revealed');
+  }
 }, {
   element: moon,
-  offset: 1000,
+  offset: 1250,
   stick: true,
   distance: 2,
   onReveal: function() {
@@ -48,6 +52,21 @@ var parallaxConfig = [{
   offset: (window.innerHeight * .5) - 30,
   onReveal: function() {
     welcome.classList.add('revealed');
+  }
+}, {
+  element: moon_arrow,
+  offset: 1100,
+  distance: 2,
+  onReveal: function() {
+    moon_arrow.classList.add('revealed');
+  }
+}, {
+  element: stick_text,
+  offset: 1850,
+  stick: true,
+  distance: 2,
+  onReveal: function() {
+    moon.classList.add('revealed');
   }
 }];
 parallaxer(parallaxConfig);
